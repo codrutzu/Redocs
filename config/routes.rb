1fe61996cf2c47f31d02ea192a9d42f15630b8a6
Codreanu Ioan-Andrei <50551781+codrutzu@users.noreply.github.com>
2023-10-20 20:25:55 +0300

    fix: sprockets

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
+
+  resources :healthcheck, only: :index
+
   namespace :api do
     namespace :v1 do
       resources :books
